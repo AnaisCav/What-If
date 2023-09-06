0fb0d8b5f05c004c6b2286a82f6c3233d1f93cc2
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-09-06 15:39:15 +0200

    Rajout fonts et polices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,19 @@ import "./index.css";
 
 function App() {
   return (
-    <div>
-      <p>hello</p>
+    <div className=" bg-accent">
+      <p className=" text-5xl font-bold font-megrim">C'est l'apocalypse</p>
+      <p className=" text-xl font-roboto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio delectus
+        quidem quia reprehenderit minus ut ad minima quo ratione? Aut enim velit
+        maxime deserunt fuga corporis laborum optio quaerat consequatur
+        repellendus quam quidem facere numquam, voluptates perspiciatis minus
+        reiciendis nihil laboriosam quae hic at. Consequatur reprehenderit
+        molestiae dicta explicabo itaque sit error esse quis similique facere,
+        animi libero consequuntur fuga soluta aut? Consequuntur cupiditate fuga
+        aliquam, deleniti tenetur commodi. Nam odio soluta recusandae, natus
+        rerum quidem at facere iste delectus.
+      </p>
     </div>
   );
 }
