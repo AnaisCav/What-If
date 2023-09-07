2c2ddd9bfe9c22717adeda8d29e99a50d9ecce86
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-09-07 09:02:40 +0200

    modif css

--- frontend/src/components/CardCommentaire.jsx
+++ frontend/src/components/CardCommentaire.jsx
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 
 const CardCommentaire = ({ user, content, creationdate }) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-between items-center">
-        <h1>{user}</h1>
+    <div className="flex flex-col text-black py-4 mt-8 lg:mt-20 mx-4 lg:mx-16 px-4 lg:px-16 text-justify border-2 border-accent rounded-2xl">
+      <div className="flex justify-between gap-8 items-center mb-8 font-megrim">
+        <h1 className="font-bold text-lg">{user}</h1>
         <p>{creationdate}</p>
       </div>
-      <div>
+      <div className="font-roboto">
         <p>{content}</p>
       </div>
     </div>
